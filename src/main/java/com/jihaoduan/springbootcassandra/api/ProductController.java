97237e63045cf859a8656a916fd0ad825e8d4b98
Jihao Duan <jihao@emparty.com>
2020-04-06 02:47:22 -0700

    add cors for all origins

--- src/main/java/com/jihaoduan/springbootcassandra/api/ProductController.java
+++ src/main/java/com/jihaoduan/springbootcassandra/api/ProductController.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -23,7 +24,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class ProductController {
 
